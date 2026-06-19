881a0c0fa669647bc9bda7fe733f0c82de762901
Oluwasegun Oluwatimileyin <boyintechgdi@gmail.com>
2026-06-19 06:59:58 +0100

    Add gdown model downloader

--- app.py
+++ app.py
@@ -2,17 +2,38 @@ from flask import Flask, render_template, request
 import joblib
 import re
 import nltk
+import os
+import gdown
+
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
 app = Flask(__name__)
 
+# Download model files from Google Drive if not present
+def download_models():
+    files = {
+        'rf_baseline.pkl': '18UKcB-AAI1xIvZkPx7vzVyhCfJAUMjwm',
+        'rf_model.pkl': '1IyGtU-y9wzGGk3WBryB8EcovHO6MGP9q',
+        'selected_features.pkl': '11YejxSlrYDoxfOPSs22mWTihY9uXm6Mf',
+        'tfidf_vectorizer.pkl': '1HjAePoTEVtQQ360m0ghh5GDFMmfcpXk5'
+    }
+    for filename, file_id in files.items():
+        if not os.path.exists(filename):
+            print(f"Downloading {filename}...")
+            gdown.download(f'https://drive.google.com/uc?id={file_id}', filename, quiet=False)
+            print(f"{filename} downloaded!")
+
+download_models()
+
+# Load all models
 rf_pso = joblib.load('rf_model.pkl')
 rf_baseline = joblib.load('rf_baseline.pkl')
 tfidf = joblib.load('tfidf_vectorizer.pkl')
 selected_features = joblib.load('selected_features.pkl')
 
+# Preprocessing
 ps = PorterStemmer()
 stop_words = set(stopwords.words('english'))
 
